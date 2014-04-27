25edeeaa85a93aaa2bf120f474eaff43a09bc9be
dipali goel <dipa.goel@gmail.com>
2014-04-27 23:44:25 +0530

    ui

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 var score=0;
+var src=['+','-','*','/','+','-','*','*','+','-'];
+var i,player_name;
 $(document).ready(function(){
     if(window.localStorage.getItem('md5'))
         {$('#login').show();}
@@ -7,10 +9,6 @@ $(document).ready(function(){
         
     $('.hometab, header').click(function(){
         homesweethome();
-    });
-    $('#homepage section').click(function(){
-    $('#homepage').hide();
-    $('#question').slideDown();
     });
     $('.answer').click(function(){
         $('.answer').css({'background-color':'red'});
@@ -22,16 +20,17 @@ $(document).ready(function(){
     });
 });
 
-var src=['+','-','*','/','+','-','*','/','+','-'];
-var i=10;
 function rockit(type){
-   
-        itseasy=setInterval(function(){--i; easy(type)},5000);
-   
-}
-function easy(type)
+            player_name=$('#player').val();
+            i=10;
+            $('#homepage').hide();
+            $('#question').slideDown();
+           fillit(type);
+           itseasy=setInterval(function(){--i; fillit(type)},5000);
+   }
+function fillit(type)
 {
-    $('.answer').css({'background-color':'transparent'});
+    $('.answer').css({'background-color':'#09455C'});
     $('.right').removeClass('right');
     opr1=Math.floor((Math.random())*10);
     opr2=Math.floor((Math.random())*10);
@@ -56,17 +55,25 @@ function easy(type)
     }
     else {
         formula=opr1+src[oprind]+opr2+src[oprind1]+opr3+src[oprind2]+opr4;
-        ambiguity=oprfalse+src[oprind]+opr2+src[oprind1]+opr3+opr3+src[oprind2]+opr4;
+        ambiguity=oprfalse+src[oprind]+opr2+src[oprind1]+opr3+src[oprind2]+opr4;
     }
     answer=eval(formula);
     fraud=eval(ambiguity);
-    $("#problem").html(formula);
+    $("#problem").html("What is "+formula+" ?");
     if(answer%2){$("#opt1").html(answer);$("#opt1").addClass('right');$("#opt2").html(fraud);}else{$("#opt1").html(fraud);$("#opt2").html(answer);$("#opt2").addClass('right');}
     
-    if (i==0){clearInterval(itseasy);alert(score);score=0;}
+    if (i==0){resulttime(type);}
 }
 
+function resulttime(type){
+    clearInterval(itseasy);
+    $("#result").html('<div class="tabs">Hi <b>'+player_name+'</b>, Score for <b>'+type+'</b> level is</div><div class="tabs">'+score+'</div>');
+    $('[data-role="page"]').hide();
+    $('#result').slideDown();
+    score=0;
+}
 function homesweethome(){
+clearInterval(itseasy);score=0;
 $('[data-role="page"]').hide();
 $('#homepage').slideDown();
 }
